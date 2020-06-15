b1b1953fe9d0cb6d219297974ed27658af865371
xwellingtonx <wellington.pires@live.com>
2020-06-15 23:48:50 +0100

    Removed building of image as base64

---
+++ client/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    chainWebpack: config => {
+      config.module
+          .rule('images')
+          .use('url-loader')
+          .loader('url-loader')
+          .tap(options => {
+              options.limit = -1
+              return options
+          })
+    }
+  }
