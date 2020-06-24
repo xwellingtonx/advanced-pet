f4d0446fc8cc8a2ac3e4239003cf1ce2e6d55eec
xwellingtonx <wellington.pires@live.com>
2020-06-24 16:58:06 +0000

    Deploying to gh-pages from  @ f0e1a0542b8b4b89238aa6bf89b5ae24656aaaf8 🚀

---
+++ js/chunk-2d0db7d5.b4a71d49.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0db7d5"],{"6fa0":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("g")},c=[],r={name:"BattleStartScene"},u=r,l=t("2877"),s=Object(l["a"])(u,a,c,!1,null,"341c0474",null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0db7d5.b4a71d49.js.map
